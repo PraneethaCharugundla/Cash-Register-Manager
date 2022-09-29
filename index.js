a05846382691ccd4cf5285a477141efcd29c2e76
Praneetha Charugundla <88439139+PraneethaCharugundla@users.noreply.github.com>
2022-09-30 00:04:47 +0530

    FIX : Update stepwise toggling and styles

--- index.js
+++ index.js
@@ -1,53 +1,75 @@
 const billAmount = document.querySelector("#bill-amount");
-const cashGiven  = document.querySelector("#cash-given");
-const checkButton  = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message1 = document.querySelector("#message-one");
+
+const message2 = document.querySelector("#message-two");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
-const availableNotes  = [2000, 500, 100 , 20 , 10 ,5 ,1 ];
+const proceedBtn = document.querySelector("#proceed");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+const toggleElement = document.querySelector("#toggle-element");
+const changeTable = document.querySelector(".change-table");
+toggleElement.style.display = "none";
 
-checkButton.addEventListener("click" , function validateBillAndCashAmount(){
- hideMessage();
- 
+function tableOpen() {
 
-  console.log(billAmount.value);
-  console.log(cashGiven.value)
+  if (billAmount.value) {
+    hideMessage1();
+    toggleElement.style.display = "block";
+    checkButton.addEventListener("click", function validateBillAndCashAmount() {
+      hideMessage2();
 
-    if(Number(cashGiven.value) >= Number( billAmount.value)  && Number(billAmount.value) > 0) {
-
-
-      console.log(cashGiven.value >= billAmount.value  && billAmount.value > 0)
-        const amountToBeReturned  = cashGiven.value - billAmount.value;
-        caluclateChange(amountToBeReturned); 
-
-    }
-    else{
-          showMessage("Do you wanna wash plates?");
-                 caluclateChange(0); 
-
-    }
+      changeTable.style.display = "none";
+      if (Number(cashGiven.value) >= Number(billAmount.value) && Number(billAmount.value) > 0) {
 
 
+        const amountToBeReturned = cashGiven.value - billAmount.value;
+        caluclateChange(amountToBeReturned);
+        changeTable.style.display = "block";
+
+      } else {
+        showMessage2("Do you wanna wash plates?");
+        changeTable.style.display = "none";
+
+      }
+    });
+  } else {
+    showMessage1("please enter valid values");
+  }
+}
+proceedBtn.addEventListener("click", tableOpen);
+changeTable.style.display = "none";
 
 
-});
- function caluclateChange (amountToBeReturned){
-   for( let i=0 ; i < availableNotes.length ; i ++) {
-       noOfNotes[i].innerHTML  = "";
-        const numberOfNotes = Math.trunc (
-            amountToBeReturned / availableNotes[i]);
 
-            amountToBeReturned %= availableNotes[i];
-            noOfNotes[i].innerText  = numberOfNotes;
-    }
+function caluclateChange(amountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    noOfNotes[i].innerHTML = "";
+    const numberOfNotes = Math.trunc(
+      amountToBeReturned / availableNotes[i]);
 
+    amountToBeReturned %= availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
   }
 
- function hideMessage(){
-    message.style.display = "none";
+}
 
- }
+function showMessage1(msg) {
+  message1.style.display = "block";
+  message1.innerText = msg;
+}
 
-function showMessage(msg){
-    message.style.display = "block";
-    message.innerText = msg;
+function hideMessage2() {
+  message2.style.display = "none";
+
+}
+
+function hideMessage1() {
+  message1.style.display = "none";
+
+}
+
+function showMessage2(msg) {
+  message2.style.display = "block";
+  message2.innerText = msg;
 }
